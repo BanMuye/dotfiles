1310e0b717d11a0b03cdc78331adaa781b1c2def
zhouchunyang <2609714703@qq.com>
2026-07-18 12:07:50 +0800

    add options and keymaps to nvim config

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,35 @@
+local opts = {
+	noremap = true -- non-recursive
+	silent = true -- do not show messages
+}
+
+-------------------
+--- Normal Mode ---
+-------------------
+
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+-- Resize with arrows
+-- delta two lines
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertial resize +2<CR>', opts)
+
+-- Better move head and tail
+vim.keymap.set('n', 'gh', '^', opts)
+vim.keymap.set('n', 'gl', '$', opts)
+
+
+
+--------------------
+--- Virtual Mode ---
+--------------------
+
+-- Move Text
+vim.keymap.set('v', '<C-j>', ":move '>+1<CR>gv=gv", opts)
+vim.keymap.set('v', '<C-k>', ":move '<-2<CR>gv=gv", opts)
---
+++ nvim/lua/options.lua
@@ -0,0 +1,23 @@
+vim.opt.clipboard = 'unnamedplus' -- use system clipboard
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' } -- pop menu even with one option without selecting
+vim.opt.mouse = 'a' -- allow use mouse
+
+-- Tab
+vim.opt.tabstop = 4 -- number of virtual spaces per Tab
+vim.opt.softtapstop = 4 -- number of spaces in tab when editing
+vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab when shifting
+vim.opt.expandtab = true -- change to spaces when tabbing
+
+-- UI config
+vim.opt.number = true -- show absolute number
+vim.opt.relativenumber = true -- show relative number
+vim.opt.cursorline = true --- highlight the cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true -- open new vertical window split below
+vim.opt.splitright = true -- open new horizontal window split right
+vim.opt.scrolloff = 2 -- keep 2 lines when scrolling vertially
+
+-- Searching
+vim.opt.insearch = true -- search as characters are entered
+vim.opt.hlsearch = true -- highlight matches
+vim.opt.ignorecase = true -- ignore cases in search by default
+vim.opt.smartcase = true -- but make it sensitive if an uppercase is entered
