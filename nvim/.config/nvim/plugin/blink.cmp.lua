ff0b97c6d1e4ccdc3c0ddb83da231ff808634ea0
zhouchunyang <2609714703@qq.com>
2026-07-26 22:20:32 +0800

    format

--- nvim/.config/nvim/plugin/blink.cmp.lua
+++ nvim/.config/nvim/plugin/blink.cmp.lua
@@ -61,13 +61,13 @@ require("blink.cmp").setup({
 			-- Show documentation automatically
 			auto_show = true,
 		},
-        
-        -- Do not select first item by default
-        list = {
-            selection = {
-                preselect = false
-            }
-        }
+
+		-- Do not select first item by default
+		list = {
+			selection = {
+				preselect = false,
+			},
+		},
 	},
 
 	-- Signature help when tying
